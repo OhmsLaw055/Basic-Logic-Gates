23177f5c1e0af1cb14c99451c2923f115c3b82ea
Shaik Abdul Khadar <abdulkhadarshaik055@gmail.com>
2025-07-19 10:08:28 +0530

    Create LogicGates.v

---
+++ src/LogicGates.v
@@ -0,0 +1,11 @@
+module AND_gate(output Y, input A, B);
+  assign Y = A & B;
+endmodule
+
+module OR_gate(output Y, input A, B);
+  assign Y = A | B;
+endmodule
+
+module NOT_gate(output Y, input A);
+  assign Y = ~A;
+endmodule
